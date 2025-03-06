862edfa02836e2c10f3c25ed9d4f3ef92154d9dd
TEMHITHORPHE <tehmhithorphe@gmail.com>
2025-03-06 04:07:40 +0100

    feat: add user types and JWT utility functions for authentication

---
+++ backend/src/types.ts
@@ -0,0 +1,21 @@
+
+
+enum Role {
+  user, admin
+}
+
+
+export interface User {
+  id: BigInt;
+  name: string;
+  email: string;
+  walletAddress: string;
+  pubKey: string;
+  role: Role;
+  createdAt: Date;
+}
+
+
+export interface JWTPayload extends User {
+  role: Role
+}
