bc2b6dcc1d16d10f9bd24c8ea8d8d8285ce82334
TEMHITHORPHE <tehmhithorphe@gmail.com>
2025-03-06 05:16:28 +0100

    feat: add mongoose and cookie-parser dependencies, enhance server setup with MongoDB connection and cookie parsing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,42 +1,60 @@
 
 import "dotenv/config";
+import path from "path";
+import mongoose from "mongoose";
 import express, { urlencoded, json } from 'express';
 import cors from 'cors';
-import routes from './routes/v1/auth.js';
-import path from "path";
+import cookieParser from 'cookie-parser';
 import { fileURLToPath } from "url";
+
+import routes from './routes/v1/auth.js';
 
 // Load environment variables
 process.loadEnvFile(".env");
+console.log(process.env.NODE_ENV);
+
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const testsDir = path.resolve(__dirname + "/../tests");
-
-
-console.log(process.env.NODE_ENV);
-
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-if (process.env.NODE_ENV === "development") app.use(express.static(testsDir));
-
 console.log(testsDir);
 
 
+
+const app = express();
+
+// setup neccesary root-level middlewares & parsers
 app.use(cors());
 app.use(urlencoded({ extended: true }));
 app.use(json());
+app.use(cookieParser());
 
+
+
+const PORT = process.env.PORT || 8080;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+    throw ("Mongo URI not found in .env file");
+}
 
 // start test page if in dev mode
 if (process.env.NODE_ENV === 'development') {
-    app.get('/', (req, res) => {
-        res.send('Hello World');
+    app.use(express.static(testsDir));
+    app.get('/mode', (req, res) => {
+        res.send('Debug mode');
     });
 }
 
 
+mongoose
+    .connect(MONGODB_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.error("MongoDB Connection Error:", err));
+
+
+// declare routes below
 app.use('/api/v1', routes);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
