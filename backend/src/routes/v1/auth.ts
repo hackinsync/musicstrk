30caa9b55915e037e7c3c57e33404fb8f94fe508
TEMHITHORPHE <tehmhithorphe@gmail.com>
2025-03-08 04:48:59 +0100

    feat: enhance authentication flow with signature verification and provider integration.

--- backend/src/routes/v1/auth.ts
+++ backend/src/routes/v1/auth.ts
@@ -1,11 +1,17 @@
 
-import { Router, Request, Response } from 'express';
-import { BigNumberish, ec, num, stark, typedData } from 'starknet';
+import { Router, Request } from 'express';
+import { BigNumberish, constants, ec, Provider, } from 'starknet';
 
-import { AUTHENTICATION_SNIP12_MESSAGE, AUTHENTICATION_SNIP12_MESSAGE_HASH } from 'constants/index';
 import { createUser } from 'models/UserModel';
-import { Role } from 'types';
+import { AUTHENTICATION_SNIP12_MESSAGE } from 'constants/index';
 import { createJWT } from 'utilities/jwt';
+import { Role } from 'types';
+
+
+const provider = new Provider({
+  nodeUrl: process.env.NODE_ENV === "production" ? process.env.STARKNET_MAINNET_RPC_API_URL : process.env.STARKNET_SEPOLIA_RPC_API_URL,
+  chainId: process.env.NODE_ENV === "production" ? constants.StarknetChainId.SN_MAIN : constants.StarknetChainId.SN_SEPOLIA
+});
 
 
 const AuthRoutes = Router();
@@ -13,15 +19,15 @@ const AuthRoutes = Router();
 
 type ReqBody_Authenticate = {
   walletAddress: BigNumberish;
-  walletPubKey: string;
+  // walletPubKey: string;
   signature: string[];
-  msgHash: string;
+  // msgHash: string;
 }
 AuthRoutes.post('/authenticate', async (req: Request<{}, {}, ReqBody_Authenticate>, res: any) => {
-  const { walletPubKey, walletAddress, signature, msgHash } = req.body;
-  console.log("[/authenticate | ReqBody]: ", walletAddress, walletPubKey, msgHash);
+  const { walletAddress, signature } = req.body;
+  // console.log("[/authenticate | ReqBody]: ", walletAddress, walletPubKey, msgHash);
 
-  if (!walletPubKey || !walletAddress || !signature) {
+  if (!walletAddress || !signature) {
     return res.status(401).json({
       error: true,
       msg: "Invalid payload received"
@@ -30,17 +36,14 @@ AuthRoutes.post('/authenticate', async (req: Request<{}, {}, ReqBody_Authenticat
 
   try {
 
-    console.log("[Signature]:", signature);
-    
-    const sig = [num.getHexString(signature[3]), num.getHexString(signature[4])];
-    console.log("[Sig]:", sig,  num.getHexString(walletPubKey));
+    console.log("[/authenticate | Signature]:", signature);
 
+    // https://docs.argent.xyz/aa-use-cases/verifying-signatures-and-cosigners#verifying-multi-signatures
+    const normalizedSignature = new ec.starkCurve.Signature(BigInt(signature[3]), BigInt(signature[4]));
+    console.log("[/authenticate | Normalized - Signature]:", normalizedSignature);
 
-
-    const isSignatureValid = typedData.verifyMessage(typedData.getMessageHash(AUTHENTICATION_SNIP12_MESSAGE, walletAddress), sig, num.getHexString(walletPubKey));
-    // const isSignatureValid = ec.starkCurve.verify(sig, msgHash, walletPubKey);
-    // const isSignatureValid = ec.starkCurve.verify(signature, msgHash, walletPubKey);
-    // console.log("[/authenticate | isValid]: ", isSignatureValid);
+    const isSignatureValid = await provider.verifyMessageInStarknet(AUTHENTICATION_SNIP12_MESSAGE, normalizedSignature, walletAddress);
+    console.log("[/authenticate | isValid]: ", isSignatureValid);
 
     if (isSignatureValid === false) {
       return res.status(401).json({
@@ -49,12 +52,12 @@ AuthRoutes.post('/authenticate', async (req: Request<{}, {}, ReqBody_Authenticat
       });
     }
 
-    throw new Error("Signature verification not implemented");
+    // throw new Error("Signature verification not implemented");
 
     // Stores wallet address in MongoDB (if not already stored).
     const user = await createUser({
       walletAddress: walletAddress.toString(),
-      pubKey: walletPubKey,
+      pubKey: walletAddress.toString(),
       email: "",
       name: "",
       role: Role.USER
@@ -69,18 +72,28 @@ AuthRoutes.post('/authenticate', async (req: Request<{}, {}, ReqBody_Authenticat
       .setHeader("Authorization", `Bearer ${authToken}`)
       // .cookie("AuthToken", authToken, { httpOnly: true, secure: true, sameSite: "strict" })
       .json({
-        msg: "Wallet Authentication Successfull"
+        msg: "Wallet Authentication Successfull",
+        token: authToken
       });
 
 
   } catch (err) {
     const error = err as Error;
     console.error("[/authenticate | Error]: ", error);
+
+    if (error.message.toLowerCase().includes("contract not found", error.message.length - 30)) {
+      return res.status(500).json({
+        error: true,
+        msg: "Contract not found (Not Deployed)"
+      });
+    }
+
     return res.status(500).json({
       error: true,
       msg: error.message
     });
   }
+
 });
 
 export default AuthRoutes;
